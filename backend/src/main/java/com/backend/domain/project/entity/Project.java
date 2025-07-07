b5560432c35ed7204f3a08d12b29df2826698ecc
ddzeun <ddzeun@gmail.com>
2025-07-07 12:56:50 +0900

    feat: 프로젝트 기본 설정 및 구조 추가

---
+++ backend/src/main/java/com/backend/domain/project/entity/Project.java
@@ -0,0 +1,35 @@
+package com.backend.domain.project.entity;
+
+import com.backend.domain.organization.entity.Org;
+import com.backend.domain.project.enums.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_project")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int projectId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "org_id")
+    private Org org;
+
+    private String name;
+    private String description;
+    private Date startAt;
+    private Date endAt;
+    private ProjectStatus status;
+
+    public int getOrgId() {
+        return org != null ? org.getOrgId() : 0;
+    }
+}
