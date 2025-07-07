757eb8e6e55fb03e7869c9a172167f761255e976
jiwonryu <jiwoni1210@naver.com>
2025-07-07 16:10:03 +0900

    refactor: 프로젝트 관련 entity import문 분리

--- backend/src/main/java/com/ourhour/domain/project/entity/IssueEntity.java
+++ backend/src/main/java/com/ourhour/domain/project/entity/IssueEntity.java
@@ -1,7 +1,13 @@
 package com.ourhour.domain.project.entity;
 
 import com.ourhour.domain.project.enums.IssueStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- backend/src/main/java/com/ourhour/domain/project/entity/ProjectParticipantId.java
+++ backend/src/main/java/com/ourhour/domain/project/entity/ProjectParticipantId.java
@@ -1,7 +1,11 @@
 package com.ourhour.domain.project.entity;
 
 import jakarta.persistence.Embeddable;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
