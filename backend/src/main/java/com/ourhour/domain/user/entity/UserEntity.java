d2de60a91e8f262e22c0f1bdbd7845b81e1e92cb
sieun <sieunuies@gmail.com>
2025-07-07 17:16:44 +0900

    feat: 계정 관련 엔티티 추가

---
+++ backend/src/main/java/com/ourhour/domain/user/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.ourhour.domain.user.entity;
+
+import com.ourhour.domain.user.enums.Platform;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="tbl_user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "userEntity")
+    private List<UserEntity> userEntityList;
+
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Platform platform;
+}
