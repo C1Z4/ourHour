6153e7f551de21846e4d355dd5f4ffa4a2d24695
sieun <sieunuies@gmail.com>
2025-07-08 17:51:01 +0900

    feat: 회사 목록 조회 (OUR-USER-18)

---
+++ backend/src/main/java/com/ourhour/domain/member/mapper/MemberOrgMapper.java
@@ -0,0 +1,24 @@
+package com.ourhour.domain.member.mapper;
+
+import com.ourhour.domain.member.dto.MemberOrgSummaryResDTO;
+import com.ourhour.domain.org.entity.OrgParticipantMemberEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MemberOrgMapper{
+
+    // 단일 OrgParticipantMemberEntity를 MemberOrgListResDTO로 매핑하는 메서드
+    @Mapping(source = "orgEntity.orgId", target = "orgId")
+    @Mapping(source = "orgEntity.name", target = "name")
+    @Mapping(source = "orgEntity.logoImgUrl", target="logoImgUrl")
+    @Mapping(source = "departmentEntity.name", target="departmentName")
+    @Mapping(source = "positionEntity.name", target="positionName")
+    MemberOrgSummaryResDTO toMemberOrgSummaryResDTO(OrgParticipantMemberEntity entity);
+
+    // OrgParticipantMemberEntity 리스트를 MemberOrgListResDTO 리스트로 매핑하는 메서드
+    List<MemberOrgSummaryResDTO> toMemberOrgSummaryResDTOList(List<OrgParticipantMemberEntity> entityList);
+
+}
--- backend/src/main/java/com/ourhour/domain/user/enums/Platform.java
+++ backend/src/main/java/com/ourhour/domain/user/enums/Platform.java
@@ -1,6 +1,7 @@
 package com.ourhour.domain.user.enums;
 
 public enum Platform {
+    OURHOUR,
     GOOGLE,
     GITHUB,
     KAKAO;
