5f3ca363f5b32b9ddf190ba9e8395c8f290b9b1f
sieun <sieunuies@gmail.com>
2025-07-09 13:49:30 +0900

    fix: MemberRepository 수정

--- backend/src/main/java/com/ourhour/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ourhour/domain/member/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package com.ourhour.domain.member.repository;
 
+import com.ourhour.domain.member.dto.MemberOrgDetailResDTO;
 import com.ourhour.domain.member.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 import com.ourhour.domain.org.entity.OrgParticipantMemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,13 +10,22 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
-    /* 회원별 회사 목록 조회 */
+    /* 본인이 속한 회사 목록 조회 */
     @Query("SELECT opm FROM OrgParticipantMemberEntity opm "
             + "JOIN FETCH opm.memberEntity m "
-            + "JOIN FETCH m.userEntity u "
             + "JOIN FETCH opm.orgEntity o "
             + "LEFT JOIN FETCH opm.departmentEntity d "
             + "LEFT JOIN FETCH opm.positionEntity p "
             + "WHERE m.memberId = :memberId")
     List<OrgParticipantMemberEntity> findOrgListByMemberId(Long memberId);
+
+    /* 본인이 속한 회사 상세 정보 조회 */
+    @Query("SELECT opm FROM OrgParticipantMemberEntity opm "
+            + "JOIN FETCH opm.memberEntity m "
+            + "JOIN FETCH opm.orgEntity o "
+            + "LEFT JOIN FETCH opm.departmentEntity d "
+            + "LEFT JOIN FETCH opm.positionEntity p "
+            + "WHERE m.memberId = :memberId AND o.orgId = :orgId")
+    OrgParticipantMemberEntity findOrgDetailByMemberIdAndOrgId(Long memberId, Long orgId);
+
 }
