d29ea7cb4cc2a1a7e9a294c8954770bf59863e50
jiwonryu <jiwoni1210@naver.com>
2025-07-10 11:27:16 +0900

    refactor(be): 프로젝트 참가자 엔티티 수정 반영

--- backend/src/main/java/com/ourhour/domain/member/entity/MemberEntity.java
+++ backend/src/main/java/com/ourhour/domain/member/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.ourhour.domain.member.entity;
 
+import com.ourhour.domain.org.entity.OrgParticipantMemberEntity;
 import com.ourhour.domain.user.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -17,11 +18,14 @@ public class MemberEntity {
     private Long memberId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private UserEntity userEntity;
 
     private String name;
     private String phone;
     private String email;
     private String profileImgUrl;
+
+    @OneToOne(mappedBy = "memberEntity", fetch = FetchType.LAZY)
+    private OrgParticipantMemberEntity orgParticipantMemberEntity;
 }
