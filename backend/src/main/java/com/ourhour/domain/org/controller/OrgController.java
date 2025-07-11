da6c240ee30d8a6159c5ff4dd5658ab8cde03159
jiwonryu <jiwoni1210@naver.com>
2025-07-11 17:32:11 +0900

    feat(be): 회사 정보 조회 api 구현(OUR-ORG-012)

--- backend/src/main/java/com/ourhour/domain/org/controller/OrgController.java
+++ backend/src/main/java/com/ourhour/domain/org/controller/OrgController.java
@@ -7,6 +7,8 @@ import com.ourhour.global.common.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,12 @@ public class OrgController {
         OrgResDTO orgResDTO = orgService.registerOrg(orgReqDTO);
 
         return ResponseEntity.ok(ApiResponse.success(orgResDTO, "팀 등록에 성공하였습니다."));
+    }
+
+    @GetMapping("/{orgId}")
+    public ResponseEntity<ApiResponse<OrgResDTO>> getOrgInfo(@PathVariable Long orgId) {
+        OrgResDTO orgResDTO = orgService.getOrgInfo(orgId);
+
+        return ResponseEntity.ok(ApiResponse.success(orgResDTO, "팀 조회에 성공하였습니다."));
     }
 }
--- backend/src/main/java/com/ourhour/domain/org/service/OrgService.java
+++ backend/src/main/java/com/ourhour/domain/org/service/OrgService.java
@@ -30,25 +30,40 @@ public class OrgService {
         OrgEntity orgEntity = orgRepository.save(orgReqEntity);
 
         // TODO: 루트 관리자 권한 추가 예정
-        /*// 루트 관리자가 될 MemberEntity 조회
-        MemberEntity memberEntity = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 ID 입니다: " + memberId));
-
-        // 해당 회사에 대한 루트 관리자 권한 생성
-        OrgParticipantMemberId orgParticipantMemberId = new OrgParticipantMemberId(orgEntity.getOrgId(), memberEntity.getMemberId());
-        OrgParticipantMemberEntity orgParticipantMemberEntity = OrgParticipantMemberEntity.builder()
-                .orgParticipantMemberId(orgParticipantMemberId)
-                .orgEntity(orgEntity)
-                .memberEntity(memberEntity)
-                .departmentEntity(null)
-                .positionEntity(null)
-                .role(Role.ROOT_ADMIN)
-                .build();
-
-        // 데이터베이스에 등록 - 해당 회사의 루트 관리자 권한
-        orgParticipantMemberRepository.save(orgParticipantMemberEntity);*/
+        /*
+         * // 루트 관리자가 될 MemberEntity 조회
+         * MemberEntity memberEntity = memberRepository.findById(memberId)
+         * .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원 ID 입니다: " +
+         * memberId));
+         * 
+         * // 해당 회사에 대한 루트 관리자 권한 생성
+         * OrgParticipantMemberId orgParticipantMemberId = new
+         * OrgParticipantMemberId(orgEntity.getOrgId(), memberEntity.getMemberId());
+         * OrgParticipantMemberEntity orgParticipantMemberEntity =
+         * OrgParticipantMemberEntity.builder()
+         * .orgParticipantMemberId(orgParticipantMemberId)
+         * .orgEntity(orgEntity)
+         * .memberEntity(memberEntity)
+         * .departmentEntity(null)
+         * .positionEntity(null)
+         * .role(Role.ROOT_ADMIN)
+         * .build();
+         * 
+         * // 데이터베이스에 등록 - 해당 회사의 루트 관리자 권한
+         * orgParticipantMemberRepository.save(orgParticipantMemberEntity);
+         */
 
         // 응답 DTO로 변환
+        OrgResDTO orgResDTO = orgMapper.toOrgResDTO(orgEntity);
+
+        return orgResDTO;
+    }
+
+    // 회사 정보 조회
+    public OrgResDTO getOrgInfo(Long orgId) {
+        OrgEntity orgEntity = orgRepository.findById(orgId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회사 ID 입니다: " + orgId));
+
         OrgResDTO orgResDTO = orgMapper.toOrgResDTO(orgEntity);
 
         return orgResDTO;
