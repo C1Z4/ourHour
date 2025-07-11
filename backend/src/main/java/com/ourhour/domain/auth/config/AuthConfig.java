ac73451ada2cc87c8cf7e3a12c4d3f854c8dc073
sieun <sieunuies@gmail.com>
2025-07-11 16:12:57 +0900

    fix : JavaMailSender 의존성 설정

--- backend/src/main/java/com/ourhour/domain/auth/config/AuthConfig.java
+++ backend/src/main/java/com/ourhour/domain/auth/config/AuthConfig.java
@@ -2,8 +2,12 @@ package com.ourhour.domain.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Properties;
 
 
 @Configuration
@@ -12,5 +16,23 @@ public class AuthConfig {
    @Bean
     public PasswordEncoder passwordEncoder() {
        return new BCryptPasswordEncoder();
+   }
+
+   @Bean
+    public JavaMailSender javaMailSender() {
+       JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+       mailSender.setHost("spring.mail.host");
+       mailSender.setPort(587);
+       mailSender.setUsername("spring.mail.username");
+       mailSender.setPassword("spring.mail.password");
+
+       Properties props = mailSender.getJavaMailProperties();
+       props.put("mail.smtp.auth", "true");
+       props.put("mail.smtp.starttls.enable", "true");
+       props.put("mail.smtp.starttls.required", "true");
+
+      mailSender.setJavaMailProperties(props);
+
+       return mailSender;
    }
 }
