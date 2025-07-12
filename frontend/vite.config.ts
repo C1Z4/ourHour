6c2a2b0b89c67158f4cd80f253313d0a86b06549
ddzeun <ddzeun@gmail.com>
2025-07-12 23:30:24 +0900

    feat(fe): vite eslint 플러그인 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-  ],
-})
+  plugins: [react(), tanstackRouter(), eslint()],
+});
