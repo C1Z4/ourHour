865d0d372da49d4bd0a0bf49ab0c4b8aa83d6fe4
Jiwon Ryu <88939170+lalabong@users.noreply.github.com>
2025-07-13 21:13:30 +0900

    feat(be): 회사 도메인 CRUD 구현 (#64)
    * feat(be): 회사 정보 조회 api 구현(OUR-ORG-012)
    
    * feat(be): 회사 정보 수정 api 구현(OUR-ORG-002)
    
    * refactor(be): 회사 정보 등록, 수정 주석 및 예외 추가
    
    * feat(be): 구성원 목록 조회 api 구현(OUR-ORG-003)
    
    * feat(be): 구성원 삭제 api 구현(OUR-ORG-006)
    
    * feat(be): 회사 삭제 api 구현(OUR-ORG-011)
    
    * feat(be): 참여 중인 프로젝트 이름 목록 조회 api 구현(OUR-ORG-008)

--- backend/src/main/java/com/ourhour/domain/org/mapper/OrgMapper.java
+++ backend/src/main/java/com/ourhour/domain/org/mapper/OrgMapper.java
@@ -3,7 +3,10 @@ package com.ourhour.domain.org.mapper;
 import com.ourhour.domain.org.dto.OrgReqDTO;
 import com.ourhour.domain.org.dto.OrgResDTO;
 import com.ourhour.domain.org.entity.OrgEntity;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface OrgMapper {
@@ -13,4 +16,10 @@ public interface OrgMapper {
 
     // Entity -> ResDTO 변환 (응답)
     OrgResDTO toOrgResDTO(OrgEntity orgEntity);
+
+    // Entity -> Entity 변환 (수정)
+    @Mapping(target = "orgId", ignore = true)
+    @Mapping(target= "orgParticipantMemberEntityList", ignore = true)
+    void updateOrgEntity(@MappingTarget OrgEntity orgEntity, OrgReqDTO orgReqDTO);
+    
 }
