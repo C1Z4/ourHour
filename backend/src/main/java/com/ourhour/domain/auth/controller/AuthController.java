1e0054096e439cc7254e130c948fcee59d94895a
sieun <sieunuies@gmail.com>
2025-07-13 23:34:28 +0900

    feat(be): JWT 토큰 생성 및 유효성 검사

--- backend/src/main/java/com/ourhour/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/ourhour/domain/auth/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.ourhour.domain.auth.controller;
 
 import com.ourhour.domain.auth.dto.SignupReqDTO;
-import com.ourhour.domain.auth.service.SignupService;
+import com.ourhour.domain.auth.service.AuthService;
 import com.ourhour.global.common.dto.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth/signup")
-public class SignupController {
+@RequestMapping("/api/auth")
+public class AuthController {
 
-    private final SignupService signupService;
+    private final AuthService authService;
 
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<ApiResponse<String>> signup(@Valid @RequestBody SignupReqDTO signupReqDTO) {
 
-        signupService.signup(signupReqDTO);
+        authService.signup(signupReqDTO);
 
         ApiResponse response = ApiResponse.success("회원가입이 완료되었습니다.");
 
         return ResponseEntity.ok(response);
     }
+
+//    @PostMapping("/signin")
+//    public ResponseEntity<ApiResponse<SigninResDTO>> signin(@Valid @RequestBody SignupReqDTO signupReqDTO) {
+//        return null;
+//    }
+//
+//    @PostMapping("/token")
+//
+//    @DeleteMapping("/token")
+
 }
