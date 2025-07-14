4fdc9d09146e75a21780858e6e95a4d912649f28
JiwonRyu <jiwoni1210@naver.com>
2025-07-14 22:55:08 +0900

    feat(fe): 개인 정보 관리 전용 왼쪽 사이드바 구현

--- frontend/src/components/common/left-sidebar/NavMain.tsx
+++ frontend/src/components/common/left-sidebar/NavMain.tsx
@@ -22,7 +22,7 @@ export function NavMain({
   items: {
     title: string;
     url: string;
-    icon?: LucideIcon;
+    icon?: LucideIcon | React.ComponentType;
     isActive?: boolean;
     items?: {
       title: string;
