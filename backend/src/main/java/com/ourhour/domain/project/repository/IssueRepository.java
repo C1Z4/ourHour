3f4fa7bd71bb004642146ef0ecb4191ecadf2d68
lalabong <jiwoni1210@naver.com>
2025-07-17 00:03:02 +0900

    feat(be): 마일스톤 진행률 계산 로직 추가 및 완료된 이슈, 총 이슈 컬럼 추가

--- backend/src/main/java/com/ourhour/domain/project/repository/IssueRepository.java
+++ backend/src/main/java/com/ourhour/domain/project/repository/IssueRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.ourhour.domain.project.entity.IssueEntity;
+import com.ourhour.domain.project.enums.IssueStatus;
 
 public interface IssueRepository extends JpaRepository<IssueEntity, Long> {
 
     Page<IssueEntity> findByMilestoneEntity_MilestoneId(Long milestoneId, Pageable pageable);
+
+    long countByMilestoneEntity_MilestoneId(Long milestoneId);
+
+    long countByMilestoneEntity_MilestoneIdAndStatus(Long milestoneId, IssueStatus status);
 }
