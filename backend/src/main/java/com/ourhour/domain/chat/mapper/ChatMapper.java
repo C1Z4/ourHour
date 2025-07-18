dd36575e023c177550812f5bc04285f8b020c462
ddzeun <ddzeun@gmail.com>
2025-07-18 11:53:48 +0900

    feat(be): 채팅방 상세 조회를 위한 API 추가(OUR-CHAT-009)

--- backend/src/main/java/com/ourhour/domain/chat/mapper/ChatMapper.java
+++ backend/src/main/java/com/ourhour/domain/chat/mapper/ChatMapper.java
@@ -3,14 +3,22 @@ package com.ourhour.domain.chat.mapper;
 import com.ourhour.domain.chat.dto.ChatMessageResDTO;
 import com.ourhour.domain.chat.dto.ChatParticipantResDTO;
 import com.ourhour.domain.chat.dto.ChatRoomListResDTO;
+import com.ourhour.domain.chat.dto.ChatRoomDetailResDTO;
 import com.ourhour.domain.chat.entity.ChatMessageEntity;
 import com.ourhour.domain.chat.entity.ChatParticipantEntity;
+import com.ourhour.domain.chat.entity.ChatRoomEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ChatMapper {
+
+    @Mapping(source = "roomId", target = "roomId")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "color", target = "color")
+    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(source = "orgEntity.orgId", target = "orgId")
+    ChatRoomDetailResDTO toChatRoomResDTO(ChatRoomEntity chatRoom);
 
     @Mapping(source = "chatRoomEntity.roomId", target = "roomId")
     @Mapping(source = "chatRoomEntity.name", target = "name")
