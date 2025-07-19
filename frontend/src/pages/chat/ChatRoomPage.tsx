d1948f16b2791ec6203a57170c3ad80d683b736d
YI JI EUN <ddzeun@gmail.com>
2025-07-19 23:31:46 +0900

    feat(fe): 채팅방 이름, 태그 색상 수정 기능 구현 (#123)
    * feat(fe): 채팅방 태그 색상 수정 기능 구현
    
    * feat(be): 채팅방 이름 변경 기능 구현

--- frontend/src/pages/chat/ChatRoomPage.tsx
+++ frontend/src/pages/chat/ChatRoomPage.tsx
@@ -2,24 +2,26 @@ import { ChatMessage } from '@/types/chatTypes.ts';
 
 import { ChatMessageInput } from '@/components/chat/room/ChatMessageInput.tsx';
 import { ChatMessageList } from '@/components/chat/room/ChatMessageList.tsx';
+import { RenamePopover } from '@/components/chat/room/RenamePopover';
 import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
+import { useChat } from '@/hooks/queries/chat/useChat';
 import { getMemberIdFromToken } from '@/utils/auth/tokenUtils';
 interface ChatRoomPageProps {
   messages: ChatMessage[];
-  sendMessage: (message: string) => void;
-  orgId: string;
-  roomId: string;
-  name: string;
+  orgId: number;
+  roomId: number;
 }
 
-export function ChatRoomPage({ messages, sendMessage, name }: ChatRoomPageProps) {
+export function ChatRoomPage({ messages, orgId, roomId }: ChatRoomPageProps) {
+  const { sendMessage } = useChat(orgId, roomId);
   const currentMemberId = getMemberIdFromToken();
+
   return (
     <div className="bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Card className="text-left mb-8 h-[70vh] flex flex-col">
           <CardHeader>
-            <h2 className="text-lg font-semibold">{name}</h2>
+            <RenamePopover orgId={orgId} roomId={roomId} />
           </CardHeader>
           <CardContent className="bg-white rounded-lg shadow-sm p-6 overflow-y-auto grow min-h-0 w-full">
             <ChatMessageList messages={messages} currentMemberId={currentMemberId} />
