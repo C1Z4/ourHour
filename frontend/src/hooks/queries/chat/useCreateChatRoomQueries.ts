ea9f267e6fd352c69248f5767c8ec00755a050d6
ddzeun <ddzeun@gmail.com>
2025-07-19 15:24:19 +0900

    feat(fe): toast 수정

--- frontend/src/hooks/queries/chat/useCreateChatRoomQueries.ts
+++ frontend/src/hooks/queries/chat/useCreateChatRoomQueries.ts
@@ -1,8 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { toast } from 'sonner';
-
 import { ChatRoomCreatePayload, createChatRoom } from '@/api/chat/chatApi';
+import { showToast } from '@/utils/toast';
 
 export const useCreateChatRoomQuery = (orgId: number) => {
   const queryClient = useQueryClient();
@@ -11,12 +10,12 @@ export const useCreateChatRoomQuery = (orgId: number) => {
     mutationFn: (payload: ChatRoomCreatePayload) => createChatRoom(orgId, payload),
 
     onSuccess: () => {
-      toast('✅ 채팅방이 성공적으로 생성되었습니다.');
-      queryClient.invalidateQueries({ queryKey: ['chatRooms', orgId] }); // 채팅방 목록 데이터를 최신으로 갱신하기 위해 무효화
+      showToast('success', '채팅방 생성에 성공하였습니다.');
+      queryClient.invalidateQueries({ queryKey: ['chatRooms', orgId] }); // 채팅방 목록 데이터를 최신으로 갱신하기 위해 무효화
     },
 
     onError: () => {
-      toast('❌ 채팅방 생성에 실패하였습니다.');
+      showToast('error', '채팅방 생성에 실패하였습니다.');
     },
   });
 };
