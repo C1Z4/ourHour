c44b37bc4fe99eaa3baa4d17f5d7d3647a22e935
YI JI EUN <ddzeun@gmail.com>
2025-07-19 23:31:46 +0900

    feat(fe): 채팅방 이름, 태그 색상 수정 기능 구현 (#123)
    * feat(fe): 채팅방 태그 색상 수정 기능 구현
    
    * feat(be): 채팅방 이름 변경 기능 구현

--- frontend/src/hooks/queries/chat/useUpdateChatRoomQueries.ts
+++ frontend/src/hooks/queries/chat/useUpdateChatRoomQueries.ts
@@ -1,8 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { toast } from 'sonner';
-
 import { ChatRoomUpdatePayload, updateChatRoom } from '@/api/chat/chatApi';
+import { showToast } from '@/utils/toast';
 
 export const useUpdateChatRoomQuery = (orgId: number, roomId: number) => {
   const queryClient = useQueryClient();
@@ -11,11 +10,12 @@ export const useUpdateChatRoomQuery = (orgId: number, roomId: number) => {
     mutationFn: (payload: ChatRoomUpdatePayload) => updateChatRoom(orgId, roomId, payload),
 
     onSuccess: () => {
-      toast('✅ 채팅방 정보가 성공적으로 수정되었습니다.');
-      queryClient.invalidateQueries({ queryKey: ['chatRooms', orgId] });
+      showToast('success', '채팅방 정보가 성공적으로 수정되었습니다.');
+      queryClient.invalidateQueries({ queryKey: ['chatRooms', roomId] });
+      queryClient.invalidateQueries({ queryKey: ['chatRoom', roomId] });
     },
     onError: () => {
-      toast('❌ 채팅방 정보 수정에 실패하였습니다.');
+      showToast('error', '채팅방 정보 수정에 실패하였습니다.');
     },
   });
 };
