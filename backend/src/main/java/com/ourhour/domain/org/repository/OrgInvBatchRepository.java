a3b73c58e970ec6d46df525358a18ea70633149b
SIEUN KIM <107247117+SerahKim@users.noreply.github.com>
2025-07-21 01:49:10 +0900

    fix(be): 권한 관련 CI 오류 수정 (#134)
    * feat(be): 초대 정보 목록 조회
    
    * fix(be): 권한 관련 CI 오류 수정
    
    * fix(be): 권한 관련 CI 오류 수정

--- backend/src/main/java/com/ourhour/domain/org/repository/OrgInvBatchRepository.java
+++ backend/src/main/java/com/ourhour/domain/org/repository/OrgInvBatchRepository.java
@@ -3,11 +3,19 @@ package com.ourhour.domain.org.repository;
 import com.ourhour.domain.org.entity.OrgInvBatchEntity;
 import com.ourhour.domain.org.entity.OrgInvEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrgInvBatchRepository extends JpaRepository<OrgInvBatchEntity, Long> {
     Long findOrgIdByBatchId(Long batchId);
 
+    @Query("""
+        select oivb
+        from OrgInvBatchEntity oivb
+        join oivb.orgParticipantMemberEntity opm
+        join opm.orgEntity o
+        where o.orgId = :orgId
+    """)
     List<OrgInvBatchEntity> findAllByOrgId(Long orgId);
 }
