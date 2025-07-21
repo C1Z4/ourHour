09c530f31347961dcca3efbddac2dc03726acfb9
YI JI EUN <ddzeun@gmail.com>
2025-07-21 14:18:56 +0900

    feat(be): 게시글 REST API 경로 수정 및 구현 (#141)

--- backend/src/main/java/com/ourhour/domain/board/repository/PostRepository.java
+++ backend/src/main/java/com/ourhour/domain/board/repository/PostRepository.java
@@ -1,9 +1,25 @@
 package com.ourhour.domain.board.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ourhour.domain.board.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
+    @Query("SELECT p " +
+            "FROM PostEntity p " +
+            "WHERE p.boardEntity.orgEntity.orgId = :orgId " +
+            "ORDER BY p.createdAt DESC")
+    List<PostEntity> findAllByOrgId(Long orgId);
+
+    @Query("SELECT p FROM PostEntity p " +
+            "WHERE p.boardEntity.boardId = :boardId " +
+            "AND p.boardEntity.orgEntity.orgId = :orgId " +
+            "ORDER BY p.createdAt DESC")
+    List<PostEntity> findPostsByBoardAndOrg(Long boardId, Long orgId);
 }
