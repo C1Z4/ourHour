d80053124030be2e9e14c52186bb9a2a8d5449aa
ddzeun <ddzeun@gmail.com>
2025-07-21 17:36:12 +0900

    feat(fe): 전체 게시글 및 게시판 별 게시글 조회 구현

---
+++ frontend/src/api/board/postApi.ts
@@ -0,0 +1,53 @@
+import { Post } from '@/types/postTypes';
+
+import axiosInstance from '../axiosConfig';
+
+export interface PostCreateUpdatePayload {
+  name: string;
+}
+
+// 전체 게시글 조회(게시판 구분 x)
+export const getAllPostList = async (orgId: number) => {
+  const response = await axiosInstance.get<Post[]>(`/api/organizations/${orgId}/boards/posts`);
+  return response.data;
+};
+
+// 게시판 별 게시글 조회
+export const getPostList = async (orgId: number, boardId: number) => {
+  const response = await axiosInstance.get<Post[]>(
+    `/api/organizations/${orgId}/boards/${boardId}/posts`,
+  );
+  return response.data;
+};
+
+// 게시글 상세 조회
+export const getPost = async (orgId: number, boardId: number, postId: number) => {
+  const response = await axiosInstance.get<Post>(
+    `/api/organizations/${orgId}/boards/${boardId}/posts/${postId}`,
+  );
+  return response.data;
+};
+
+// 게시글 등록
+export const createPost = async (
+  orgId: number,
+  boardId: number,
+  payload: PostCreateUpdatePayload,
+) => {
+  await axiosInstance.post(`/api/organizations/${orgId}/boards/${boardId}/posts`, payload);
+};
+
+// 게시글 수정
+export const updatePost = async (
+  orgId: number,
+  boardId: number,
+  postId: number,
+  payload: PostCreateUpdatePayload,
+) => {
+  await axiosInstance.put(`/api/organizations/${orgId}/boards/${boardId}/posts/${postId}`, payload);
+};
+
+// 게시글 삭제
+export const deletePost = async (orgId: number, boardId: number, postId: number) => {
+  await axiosInstance.delete(`/api/organizations/${orgId}/boards/${boardId}/posts/${postId}`);
+};
