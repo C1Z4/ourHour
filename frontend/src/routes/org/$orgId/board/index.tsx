9591df60c3c63b75a2a94c62261935cd82127808
ddzeun <ddzeun@gmail.com>
2025-07-21 17:29:41 +0900

    feat(fe): 게시판 목록 조회 화면 구현

--- frontend/src/routes/org/$orgId/board/index.tsx
+++ frontend/src/routes/org/$orgId/board/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { useBoardListQuery } from '@/hooks/queries/board/useBoardListQuery';
+import { BoardListPage } from '@/pages/board/BoardListPage';
 export const Route = createFileRoute('/org/$orgId/board/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello &quot;/$orgId/board/&quot;!</div>;
+  const { orgId } = Route.useParams();
+  const numOrgId = Number(orgId);
+  const { data: boardList = [] } = useBoardListQuery(numOrgId);
+
+  return <BoardListPage orgId={numOrgId} boardList={boardList} />;
 }
