a784cbbcb35582220b965d57b61431c321a35b63
JiwonRyu <jiwoni1210@naver.com>
2025-07-23 00:00:40 +0900

    chore(be): commentDTO profileImg -> profileImgUrl 수정

--- backend/src/main/java/com/ourhour/domain/comment/dto/CommentDTO.java
+++ backend/src/main/java/com/ourhour/domain/comment/dto/CommentDTO.java
@@ -14,7 +14,7 @@ public class CommentDTO {
     private Long commentId;
     private Long authorId;
     private String name;
-    private String profileImg;
+    private String profileImgUrl;
     private String content;
     private LocalDateTime createdAt;
     private List<CommentDTO> childComments;
