264bca6e26f9d93017dddce0febd33c87028702e
ddzeun <ddzeun@gmail.com>
2025-07-22 11:09:10 +0900

    feat(fe): 각 게시판의 게시글 가장 하단에 구분선 생기도록 TableRow 추가

--- frontend/src/components/board/BoardCard.tsx
+++ frontend/src/components/board/BoardCard.tsx
@@ -54,12 +54,15 @@ export const BoardCard = ({ orgId, boardId, board }: Props) => {
             }
 
             return postList.slice(0, 5).map((post) => (
-              <TableRow key={post.postId} className="cursor-pointer hover:bg-muted/50">
-                <TableCell className="font-medium truncate">{post.title}</TableCell>
-                <TableCell className="text-right text-sm text-muted-foreground w-[120px]">
-                  {post.createdAt.substring(0, 10)}
-                </TableCell>
-              </TableRow>
+              <>
+                <TableRow key={post.postId} className="cursor-pointer hover:bg-muted/50">
+                  <TableCell className="font-medium truncate">{post.title}</TableCell>
+                  <TableCell className="text-right text-sm text-muted-foreground w-[120px]">
+                    {post.createdAt.substring(0, 10)}
+                  </TableCell>
+                </TableRow>
+                <TableRow />
+              </>
             ));
           })()}
         </TableBody>
