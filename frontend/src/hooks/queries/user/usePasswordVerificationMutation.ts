45e1656082165238f186179ca432fda347d22c72
lalabong <jiwoni1210@naver.com>
2025-07-23 19:13:54 +0900

    fix(fe): 누락된 api 호출 함수 추가

--- frontend/src/hooks/queries/user/usePasswordVerificationMutation.ts
+++ frontend/src/hooks/queries/user/usePasswordVerificationMutation.ts
@@ -2,15 +2,13 @@ import { useMutation } from '@tanstack/react-query';
 
 import { AxiosError } from 'axios';
 
-import postPasswordVerification, {
-  PostPasswordVerificationRequest,
-} from '@/api/auth/postPwdVerification';
+import { postVerifyPwd, PostVerifyPwdRequest } from '@/api/user/postVerifyPwd';
 import { getErrorMessage, handleHttpError, logError } from '@/utils/auth/errorUtils';
 import { showErrorToast } from '@/utils/toast';
 
 const usePasswordVerificationMutation = () =>
   useMutation({
-    mutationFn: (request: PostPasswordVerificationRequest) => postPasswordVerification(request),
+    mutationFn: (request: PostVerifyPwdRequest) => postVerifyPwd(request),
     onError: (error: AxiosError) => {
       logError(error);
       handleHttpError(error);
