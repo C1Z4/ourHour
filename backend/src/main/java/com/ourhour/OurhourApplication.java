b59071c6e34bc3b1ecfb382b4dd6bb179f4690f1
lalabong <jiwoni1210@naver.com>
2025-08-11 13:19:07 +0900

    feat(be): aop 사용을 위한 의존성 추가

--- backend/src/main/java/com/ourhour/OurhourApplication.java
+++ backend/src/main/java/com/ourhour/OurhourApplication.java
@@ -2,10 +2,12 @@ package com.ourhour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = false)
 public class OurhourApplication {
 
     public static void main(String[] args) {
