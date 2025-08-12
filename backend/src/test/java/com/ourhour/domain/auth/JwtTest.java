588e8cfbef46e8b2fa8305dc2b37096fbf192f5c
SIEUN KIM <107247117+SerahKim@users.noreply.github.com>
2025-08-12 15:39:52 +0900

    feat: application-test.yml 테스트용 profile 분리 (#211)
    * fix: test 및 build 분리
    
    * fix: test 및 build 분리
    
    * feat: application-test.yml 테스트용 profile 분리

--- backend/src/test/java/com/ourhour/domain/auth/JwtTest.java
+++ backend/src/test/java/com/ourhour/domain/auth/JwtTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class JwtTest {
 
