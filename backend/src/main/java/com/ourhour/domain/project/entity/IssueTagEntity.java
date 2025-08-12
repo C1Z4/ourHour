85f740a9c0221cc29863ccedb336eabe7ddd70e2
lalabong <jiwoni1210@naver.com>
2025-08-12 17:21:26 +0900

    feat(be): 이슈 태그 entity에 projectId 추가

--- backend/src/main/java/com/ourhour/domain/project/entity/IssueTagEntity.java
+++ backend/src/main/java/com/ourhour/domain/project/entity/IssueTagEntity.java
@@ -2,20 +2,30 @@ package com.ourhour.domain.project.entity;
 
 import com.ourhour.global.common.enums.TagColor;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tbl_issue_tag")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class IssueTagEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "issue_tag_id")
     private Long issueTagId;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private ProjectEntity projectEntity;
+
+    @Setter
     private String name;
 
+    @Setter
     @Enumerated(EnumType.STRING)
     private TagColor color;
 }
