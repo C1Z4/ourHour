3c156242adeeeafeeb2bcf080da1dfe541071064
sieun <sieunuies@gmail.com>
2025-08-12 15:35:58 +0900

    feat: application-test.yml 테스트용 profile 분리

--- backend/src/test/java/com/ourhour/domain/auth/AuthTestController.java
+++ backend/src/test/java/com/ourhour/domain/auth/AuthTestController.java
@@ -4,11 +4,13 @@ import com.ourhour.domain.org.enums.Role;
 import com.ourhour.global.jwt.annotation.OrgAuth;
 import com.ourhour.global.jwt.annotation.OrgId;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ActiveProfiles("test")
 @RestController
 @RequestMapping("/api/test")
 public class AuthTestController {
