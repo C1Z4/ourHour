29457d5a6cc7c280462dbacbd588cfa5bff003e5
lalabong <jiwoni1210@naver.com>
2025-08-14 22:01:29 +0900

    feat(be): 부서 및 직책 엔티티 확장

--- backend/src/main/java/com/ourhour/domain/org/entity/PositionEntity.java
+++ backend/src/main/java/com/ourhour/domain/org/entity/PositionEntity.java
@@ -2,8 +2,10 @@ package com.ourhour.domain.org.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +14,21 @@ import java.util.List;
 @Table(name = "tbl_position")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class PositionEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long positionId;
 
-    @OneToMany(mappedBy="positionEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "positionEntity", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrgParticipantMemberEntity> orgParticipantMemberEntityList = new ArrayList<>();
 
     private String name;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "org_id", nullable = false)
+    private OrgEntity orgEntity;
 
-    public void changePosition(String positionName) {
-        if (positionName!=null) this.name =positionName;
-    }
 }
