ebe5f6d81c2fbe0c83fc629fe5812549790516b1
lalabong <jiwoni1210@naver.com>
2025-08-22 13:10:41 +0900

    feat(be): org 관련 dto에 필요한 어노테이션들 추가

--- backend/src/main/java/com/ourhour/domain/member/dto/MemberInfoResDTO.java
+++ backend/src/main/java/com/ourhour/domain/member/dto/MemberInfoResDTO.java
@@ -1,12 +1,14 @@
 package com.ourhour.domain.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberInfoResDTO {
