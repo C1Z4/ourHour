d6a47440b3092962b263b6ea7ce37206438f9666
lalabong <jiwoni1210@naver.com>
2025-08-26 21:40:38 +0900

    feat(fe): 새로고침시 인증 상태 초기화 함수 주석해제

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,11 +9,13 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 
+import '@/styles/index.css';
+
+import { store } from '@/stores/store';
+import { restoreAuthFromServer } from '@/utils/auth/tokenUtils';
+
 import 'react-toastify/dist/ReactToastify.css';
 import { routeTree } from './routeTree.gen';
-import { store } from './stores/store';
-import './styles/index.css';
-// import { restoreAuthFromServer } from './utils/auth/tokenUtils';
 
 const router = createRouter({ routeTree });
 
@@ -39,7 +41,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
 
   // 앱 시작 시 인증 상태 초기화
-  // restoreAuthFromServer();
+  restoreAuthFromServer();
 
   root.render(
     // <StrictMode>
